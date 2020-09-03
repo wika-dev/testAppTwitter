087622ffa01fed23569aa1bdef4123ebf9b18907
Michael <contact@michael-mercier.com>
2020-09-03 14:49:02 +0200

    fix

--- environment/production.js
+++ environment/production.js
@@ -2,6 +2,6 @@
 
 module.exports = {
     dbUrl: 'mongodb+srv://mike:azerty@clustertest.0zw31.gcp.mongodb.net/twitter?retryWrites=true&w=majority',
-    cert: '',
-    key: ''
+    cert: '/etc/letsencrypt/live/www.apteryx.dev/fullchain.pem',
+    key: '/etc/letsencrypt/live/www.apteryx.dev/privkey.pem'
 }
